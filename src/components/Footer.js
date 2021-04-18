6c23040cb84de3f56c4437b38c2f1bee0084125b
Dina <dmuhic@gmail.com>
2021-04-18 13:26:54 +0200

    render services dynamically

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,9 @@ import React from 'react';
 
 const Footer = ({content}) => {
     const social = content.filter(item => item.url);
-    const renderSocial = social.map(item => {
-        return <a className='btn' href={item.url} target="_blank" rel="noreferrer" > <i className={`fab fa-3x  fa-${item.name}`}></i></a>
-        // switch (item.name) {
-        //     case 'linkedin':
-        //         return <a className='btn' href={item.url} target="_blank" rel="noreferrer" > <i className='fab fa-3x  fa-linkedin'></i></a>;
-        //     case 'facebook':
-        //         return <a className='btn' href={item.url} target="_blank" rel="noreferrer" > <i className='fab fa-3x  fa-facebook'></i></a>;
-        //     case 'instagram':
-        //         return <a className='btn' href={item.url} target="_blank" rel="noreferrer" > <i className='fab fa-3x  fa-instagram'></i></a>;
-        //     case 'twitter':
-        //         return <a className='btn' href={item.url} target="_blank" rel="noreferrer" > <i className='fab fa-3x  fa-twitter'></i></a>;
-        //     default:
-        //         return <i></i>
-        // }
+    const renderSocial = social.map((item, index) => {
+        return <a className='btn' key={index} href={item.url} target="_blank" rel="noreferrer" > <i className={`fab fa-3x  fa-${item.name}`}></i></a>
+       
     });
 
     return (
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
+const Services = ({content}) => {
 
-const Services = () => {
+    // renders content name in title, description array as paragraphs
+    // receives object with service details and index for use as key
+    const renderContent = (service, key) => {
+        return (
+            <div className='services-content' key={key}>
+                <h4>{service.name}</h4>
+                <p className='price'>{service.price}</p>
+                {service.description.map((item, index) => {
+                    return <p key={key + '-' + index}>{item}</p>;
+                })}
+            </div>
+        );
+    }
+    
+    const services = content.map((item, index) => {
+        return (item.description) 
+            // if item has description, render content
+            ? renderContent(item, index)
+            // if item doesn't have description, retrieve nested content
+            : (
+                <div className='services-package' key={index}>
+                    <h3>{item.name}</h3>
+                    {item.content.map((cItem, cIndex) => {
+                        const key = index + '-' + cIndex;
+                        return renderContent(cItem, key);
+                    })}
+                </div>
+            )
+    });
+
     return (
         <div>
-           
+           {services}
         </div>
     );
 }
