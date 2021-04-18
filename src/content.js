e1de32561ceb6c7f2b4937b9514a14370c576464
Dina <dmuhic@gmail.com>
2021-04-18 12:49:42 +0200

    add content from client

--- src/content.js
+++ src/content.js
@@ -18,46 +18,51 @@ const homeContent = {
 const servicesContent = [
     {
         name: "Copyediting",
+        price: "$225",
         description: [
             "Do you have a complete (or nearly complete) piece of writing that you want to ensure is clear, coherent, and error-free? Copyediting identifies and corrects grammatical and syntactical errors, typos, misspellings, formatting mistakes, and other confusions or inconsistencies in documents that are final drafts, close to being submitted.",
+
             "In writing classes, this stage is often called “polishing” your work. When the quality of your ideas is already there, copyediting makes your writing shine."
         ]
     },
     {
         name: "Developmental editing",
-        description: "Are you working on a draft (or drafts) of a document that you want to improve substantially before your writing reaches its final form?
+        price: "$225",
+        description: [
+            "Are you working on a draft (or drafts) of a document that you want to improve substantially before your writing reaches its final form?",
 
- 
+            "Developmental editing provides deeper layers of guidance and feedback throughout the writing process. It is the best form of editing for writers who are still looking to clarify their ideas, make decisions about structure and organization, or for writers who are seeking a breakthrough that can take their work to a new level of sophistication, intelligence, or depth.",
 
-        Developmental editing provides deeper layers of guidance and feedback throughout the writing process. It is the best form of editing for writers who are still looking to clarify their ideas, make decisions about structure and organization, or for writers who are seeking a breakthrough that can take their work to a new level of sophistication, intelligence, or depth.
-        
-         
-        
-        Developmental editing is more collaborative than copyediting. My feedback is more substantial and more individual. Developmental editing can be especially helpful for writers who feel stuck or stagnant, trapped in a negative space with their work."
+            "Developmental editing is more collaborative than copyediting. My feedback is more substantial and more individual. Developmental editing can be especially helpful for writers who feel stuck or stagnant, trapped in a negative space with their work."
+        ]   
     },
     {
         name: "Writing Tutoring and Coaching",
-        description: ""
+        price: "$225",
+        description: [
+            ""
+        ]
     },
     {
         name: "Editing packages",
-        description: "Application Essay Package: $225
+        content: [
+            {   
+                name: "Application Essay Package",
+                price: "$225",
+                description: [
+                    "A 30-minute one-on-one consultation",
+                    "Initial developmental edit of an application essay or essays (up to 1500 words)",
+                    "Final copyedit of application essay or essays (up to 1500 words)"
+                ]
+            },
+        ]
+    }
 
-        o   A 30-minute one-on-one consultation
-        
-        o   Initial developmental edit of an application essay or essays (up to 1500 words)
-        
-        o   Final copyedit of application essay or essays (up to 1500 words)"
-    },
-    {
-        name: "Other",
-        description: ""
-    },
 ];
 
 /*  TO EDIT ABOUT CONTENT, INDIVIDUAL PARAGRAPHS MUST BE WRAPPED IN QUOTES AND SEPARATED WITH COMMA */
 const aboutContent = [
-    "Margaret Bostrom, PhD (she/her",
+    "Margaret Bostrom, PhD (she/her)",
     "I am an editor, writer, and teacher with nearly two decades of experience across a wide variety of editorial projects.",
     "I have worked with self-published authors, print & web publications, textbook publishers, undergraduate and graduate students, career academics, and working professionals."
 ];
